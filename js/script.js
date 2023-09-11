593180c6e703f2dc8d2dd64805542696b81a4c3e
Carla Vidales <carlavidales.ca@gmail.com>
2023-09-11 13:47:46 -0700

    Finish creating quotes array

--- js/script.js
+++ js/script.js
@@ -12,12 +12,37 @@ project 1 - A Random Quote Generator
 ***/
 const quotes = [
   {
-    // object 1
+    quote: "The flower that blooms in adversity is the most rare and beautiful of all.",
+    source: "The Emperor",
+    citation: "Mulan",
+    year: 1998,
   },
   {
-    // object 2
-  }
-]
+    quote: "Our fate lives within us. You only have to be brave enough to see it.",
+    source: "Merida",
+    citation: "Brave",
+    year: 2012,
+  },
+  {
+    quote: "A true hero isn't measured by the size of his strength, but by the strength of his heart.",
+    source: "Zeus",
+    citation: "Hercules",
+    year: 1997,
+  },
+  {
+    quote: "You're braver thann you believe, and stronger than you seem, and smarter than you think.",
+    source: "Winnie the Pooh",
+    citation: "Pooh's Most Grand Adventure",
+    year: 1997,
+  },
+  {
+    quote: "Remember, you're the one who can fill the world with sunshine.",
+    source: "Snow White",
+    citation: "Snow White and the Seven Dwarves",
+    year: 1937,
+  },
+
+];
 
 
 /***
